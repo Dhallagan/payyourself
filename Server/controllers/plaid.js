b2e5e5a2ed8f49fbedb7d2b2d9dc97dcbcd3c946
ahw <andrewhallagan@gmail.com>
2017-05-27 21:12:09 -0400

    Clean up Plaid client

--- Server/controllers/plaid.js
+++ Server/controllers/plaid.js
@@ -4,19 +4,7 @@ const client_id = process.env.CLIENT_ID;
 const secret = process.env.SECRET;
 const public_key = process.env.PUBLIC_KEY;
 const plaid_env = process.env.PLAID_ENV;
-const options = ""
 
-const plaidClient = new plaid.Client(client_id, secret, public_key, plaid_env, options);
+const plaidClient = new plaid.Client(client_id, secret, public_key, plaid_env);
 
 module.exports = plaidClient;
-
-
-// plaidClient.exchangePublicToken(public_token, function(err, res) {
-//   const access_token = res.access_token;
-// 
-//   console.log(access_token)
-//   });
-// 
-// plaidClient.getAccounts(access_token, function(err, res) {
-//    console.log(res.accounts);
-// });
